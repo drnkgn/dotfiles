7ca6eba9097eccf09a5bfe20f4d9777a9573777d
drnkgn <jameswong9562@gmail.com>
2023-08-24 10:38:23 +0800

    Added wezterm configs

---
+++ wezterm/tabline.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+M.colorscheme = {
+    everforest = {
+        background = "#323C41",
+        active_tab = {
+            bg_color = "#a7c080",
+            fg_color = "#2b3339",
+            intensity = "Bold",
+        },
+        inactive_tab = {
+            bg_color = "#445055",
+            fg_color = "#859289",
+        },
+        new_tab = {
+            bg_color = "#a7c080",
+            fg_color = "#2b3339",
+        },
+        inactive_tab_hover = {
+            bg_color = "#445055",
+            fg_color = "#859289",
+        },
+        new_tab_hover = {
+            bg_color = "#a7c080",
+            fg_color = "#2b3339",
+        }
+    },
+    kanagawa = {
+        background = "#1F1F28",
+        active_tab = {
+            bg_color = "#1F1F28",
+            fg_color = "#C8C093",
+            intensity = "Bold",
+        },
+        inactive_tab = {
+            bg_color = "#16161D",
+            fg_color = "#727169",
+        },
+        new_tab = {
+            bg_color = "#252535",
+            fg_color = "#7E9CD8",
+        },
+        inactive_tab_hover = {
+            bg_color = "#16161D",
+            fg_color = "#727169",
+        },
+        new_tab_hover = {
+            bg_color = "#2D4F67",
+            fg_color = "#7E9CD8",
+        }
+    }
+}
+
+return M
