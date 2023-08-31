b90ca7fae567ddcd54fb65802b8c3b216db82369
drnkgn <jameswong9562@gmail.com>
2023-09-01 00:30:32 +0800

    Added QOL remaps

--- nvim/lua/mapping.lua
+++ nvim/lua/mapping.lua
@@ -1,8 +1,13 @@
 local builtin = require("telescope.builtin")
 
-local set = vim.keymap.set
+-- append line without moving cursor
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- shift visual up and down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 -- telescope.nvim
-set("n", "<leader>ff", builtin.find_files)
-set("n", "<leader>fb", builtin.buffers)
-set("n", "<leader>fg", builtin.live_grep)
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep)
