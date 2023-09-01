f0aea56bdce82f7e4618b5efc04c8616cbd7de04
drnkgn <jameswong9562@gmail.com>
2023-09-01 11:03:37 +0800

    Buncha fixes and stuff changed
    - fixed vim.lsp.buf.format by wrapping in a function
    - added permissions, size and mtime to oil.nvim
    - removed vim-startuptime
    - removed code that disables netrw, oil does that by default
    - `ts` and `js` files does 4 space of indents

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -60,14 +60,16 @@ cmp.setup({
 local custom_attach = function(client, bufnr)
   vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-  local opts = { buffer = bufnr }
+  local opts = { buffer=bufnr }
   vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, opts)
   vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
   vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
   vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format({ async=true }), opts)
+  vim.keymap.set('n', '<leader>fm', function()
+    vim.lsp.buf.format({ async=true })
+  end, opts)
   vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
   vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,9 +5,6 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- VIM OPTIONS --
 
 vim.o.updatetime = 50
